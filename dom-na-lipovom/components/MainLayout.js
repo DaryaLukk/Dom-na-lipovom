b06f60c8a27ad105121230941edc4a815353794a
darya <lukina-dasha@mail.ru>
2024-12-28 10:51:09 +0300

    яндекс метрика

--- dom-na-lipovom/components/MainLayout.js
+++ dom-na-lipovom/components/MainLayout.js
@@ -30,6 +30,29 @@ export function MainLayout({ children }) {
         <meta property="og:title" content="Дом на Липовом | Посуточная аренда гостевых домов в Тюмени" />
         <meta property="og:description" content="Посуточная аренда гостевых домов в Тюмени на Липовом озере. Панорамные окна, кухня с современной техникой, баня и терраса. Близость к городу, аквапарку и горячим источникам.'"/>
         <meta property="og:image" content="/pictures/carousel2.jpeg" />
+        {/* Скрипт Яндекс.Метрики */}
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(m,e,t,r,i,k,a){
+                m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                m[i].l=1*new Date();
+                for (var j = 0; j < document.scripts.length; j++) {
+                  if (document.scripts[j].src === r) { return; }
+                }
+                k=e.createElement(t),a=e.getElementsByTagName(t)[0],
+                k.async=1,k.src=r,a.parentNode.insertBefore(k,a);
+              })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+              ym(99344852, "init", {
+                clickmap:true,
+                trackLinks:true,
+                accurateTrackBounce:true
+              });
+            `,
+          }}
+        />
       </Head>
       <nav>
         <div 
@@ -60,6 +83,11 @@ export function MainLayout({ children }) {
         </div>
       </nav>
       <main>
+        <noscript>
+          <div>
+            <img src="https://mc.yandex.ru/watch/99344852" style={{ position: 'absolute', left: '-9999px' }} alt="" />
+          </div>
+        </noscript>
         <div className={menu ? 'menu vis' : 'menu hid'}>
           <div className="content">
             <div className="logo">
